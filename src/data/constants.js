ea687e7320c17d8d307546c32cc1e7b089bc1510
arun-mn <mnarun57@gmail.com>
2024-07-15 13:00:38 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,208 @@
+export const Bio = {
+  name: "Arun M N",
+  roles: [
+    "Full Stack Developer",
+    "Back end Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/arun-devappsys",
+  resume:
+    "https://drive.google.com/file/d/1xyrhuHYzf5TQA_2aUbDtdz22rVFi1MR-/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/arun-m-n-27b568318",
+  insta: "https://www.instagram.com/mr___arun_17?igsh=MnQzczdlaWxhMGQ=",
+  facebook: "https://www.facebook.com/arun.mn.524?mibextid=ZbWKwL"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://logos-download.com/wp-content/uploads/2016/10/Java_logo_icon.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.3lerszXonPy2hvSP1jkh9wHaHK&pid=Api&P=0&h=180",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.p_mVTStF7NkQg0_rI2kkPwAAAA&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C510BAQG_wT4h8KGvjg/company-logo_200_200/0/1631318876472?e=2147483647&v=beta&t=tfMmBgxlJsVdxdFGYChf8jE8UC-EtGcjK9L54kP6OjM",
+    role: "Software Developer as a Intern",
+    company: "DevAppSys IT Solutions Pvt. Ltd.",
+    date: "May 2024 - Present",
+    desc: "Working on the backend of the web application using Spring Boot and streamlining the process with automation.",
+    skills: [
+      "Git",
+      "GitHub",
+      "Spring Boot",
+      "SQL",
+      "Java",
+      "MySql",
+      "SQLyog"
+    ],
+
+  },
+  {
+    id: 0,
+    img: "https://lh3.googleusercontent.com/fTDq8f2CEFf2t1TXMZj1B2g16UO4h3EshxBYi_T86snawDp0kaNy48Y9HEsOMAYMfSrGbZu-CTvG4puzSadZlkZTDAOlIzU=s240",
+    role: "Full Stack Developer as Intern",
+    company: "AgileMinds",
+    date: "Jan 2024 - Apr 2024",
+    desc: "Working on the both frontend and backend of the web application using ReactJS, Net Beans, and Java.",
+    skills: [
+      "ReactJS",
+      "MySql",
+      "DB solo",
+      "HTML",
+      "CSS",
+      "Java",
+      "JavaScript",
+      "Net Beans",
+      "Tomcat",
+    ],
+    
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media-exp1.licdn.com/dms/image/C510BAQHH1K_Dj-BPtg/company-logo_200_200/0?e=2159024400&v=beta&t=AlOjWynHoSFnq9I4Ovfp9vfoDb6sUsahGzpGioHOfcI",
+    school: "Karnataka (Govt) Polytechnic Mangalore",
+    date: "Oct 2021 - Aug 2024",
+    grade: "8.94 CGPA",
+    desc: "I completed my diploma education at Karnataka (Govt) Polytechnic College with a CGPA of 8.94. I have taken courses in Data Structures, Algorithms,  Database Management Systems, Operating Systems, and Computer Networks, among others. ",
+    degree: "Diploma in Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://tse1.mm.bing.net/th?id=OIP.zXDs8ny-qx2AWebZ00VHjAAAAA&pid=Api&P=0&h=180",
+    school: "Besant National PU College",
+    date: "June 2018 - Apr 2020",
+    grade: "75.50%",
+    desc: "I completed my 2nd PU education at Besant NAtional PU College, Mangalore, where I studied Commerce with Computer Science.",
+    degree: "Commerce in Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://tse4.explicit.bing.net/th?id=OIP.91uV8JtBD4Ttvz51NDMtuwHaHR&pid=Api&P=0&h=180",
+    school: "Lourdes High School,  Bejai",
+    date: "June 2017 - Apr 2018",
+    grade: "83.20%",
+    desc: "I completed my SSLC education at Lourdes High School, Bejai.",
+    degree: "ICSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "SprintVC",
+    date: "May 2024 - Jul 2024",
+    description:
+      "Sprint provide an alternative investment platform that is accessible to all, regardless of financial status. It founded with the aim to democratise fundraising and expand angel investing to the masses.",
+    image:
+      "https://sprintvc.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1f2c692c.png&w=256&q=75",
+    tags: [
+      "MySql",
+      "React Js",
+      "Java",
+      "Spring Boot",
+      "SQLyog",
+    ],
+    category: "web app",
+    webapp: "https://sprintvc-inv.uatdemo.info/",
+  },
+  {
+    id: 9,
+    title: "Energy Sync",
+    date: "Jan 2024 - Apr 2024",
+    description:
+      "Energy Sync is a cloud-based platform that helps you manage your energy and water consumption and costs. Log in to your account and access your dashboard.",
+    image:
+      "https://grafana.com/static/assets/img/blog/homeenergydashboard.png",
+    tags: [
+      "MySql",
+      "Java",
+      "Net Beans",
+      "Tomcat",
+      "Ice Face",
+      "DB Solo",
+    ],
+    category: "web app",
+    webapp: "http://fair.mesco.in",
+  },
+];
